635bcf16bcd0b25bbad2ed83414c93d80486a860
daniel-rachfal <rachfaldaniel@gmail.com>
2024-05-12 13:56:47 +0100

    Added info text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <>
       <Text.H1>Home</Text.H1>
+      <p>
+        This website was created for a take home assignment and is in no way
+        affiliated with SpaceX. If you have come across the website through the
+        power of search engines, you will likely not find much of use here.
+      </p>
     </>
   );
 }

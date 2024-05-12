2760796033d7904530e98b7bcd5488af78b160cd
daniel-rachfal <rachfaldaniel@gmail.com>
2024-05-12 14:01:19 +0100

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="bg-gray-900 text-white min-h-dvh px-8 lg:px-40 py-4">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-center bg-gray-900">
+        This website is not affiliated, associated, authorized, endorsed by, or
+        in any way officially connected with Space Exploration Technologies Corp
+        (SpaceX), or any of its subsidiaries or its affiliates. The names SpaceX
+        as well as related names, marks, emblems and images are registered
+        trademarks of their respective owners.
+      </p>
+    </footer>
+  );
+}

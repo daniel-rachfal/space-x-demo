19ece15c776f02070bf1370fe097a97be21dc463
daniel-rachfal <rachfaldaniel@gmail.com>
2024-06-27 17:27:41 +0100

    Refactor dragons using clsx

--- src/components/dragons/index.tsx
+++ src/components/dragons/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { getAllDragons } from "../../services/spacexapi";
 import TimelineIcon from "./TimelineIcon";
 
@@ -13,9 +14,11 @@ export default async function DragonsTimeline() {
           </div>
           <div
             // Add classes based on index to alternate the timeline
-            className={`timeline-${index % 2 === 0 ? "start" : "end"} lg:text-${
-              index % 2 === 0 ? "right" : "left"
-            } mb-10 mx-2`}
+            className={clsx(
+              "mb-10 mx-2",
+              `timeline-${index % 2 === 0 ? "start" : "end"}`,
+              `lg:text-${index % 2 === 0 ? "right" : "left"}`
+            )}
           >
             <time className="font-mono italic">
               First Flight: {dragon.first_flight}
